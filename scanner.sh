4ece1d39e0f8bb295edf359adf240870b28cb978
AlexRandomed <62905495+AlexRandomed@users.noreply.github.com>
2021-02-16 17:00:06 +0100

    Create scanner.sh

---
+++ scanner.sh
@@ -0,0 +1,1 @@
+export ip=192.168.1.254; for port in $(seq 1 65535); do timeout 0.01 bash -c "</dev/tcp/$ip/$port && echo Port $port is open || echo Port $port is closed > /dev/null" 2>/dev/null || echo Connection Timeout > /dev/null; done | grep Port
