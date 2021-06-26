83bdee3b5d12ae473c17d2784ee8d5bdf6a482a4
AlexRandomed <62905495+AlexRandomed@users.noreply.github.com>
2021-06-26 13:56:00 +0200

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -5,6 +5,7 @@ GREEN=`tput setaf 10`
 BLUE=`tput setaf 87`
 RED=`tput setaf 1`
 port_number=$2
+precision=$3
 
 function scanner()
 {
@@ -12,13 +13,18 @@ function scanner()
         BLUE=`tput setaf 87`
         port_number=$2
         ip=$1
-
+        precision=$3
+        if [ -z $precision ]; 
+                then precision=0.05; 
+                else echo "Precision is set to $precision"; 
+        fi
         for port in $(seq 1 $port_number);
         do
-                timeout 0.01 bash -c "</dev/tcp/$ip/$port && echo ${GREEN}[+] Port $port is open || echo Port $port is closed > /dev/null" 2>/dev/null || echo Connection timeout > /dev/null
+                timeout $precision bash -c "</dev/tcp/$ip/$port && echo ${GREEN}[+] Port $port is open || echo Port $port is closed > /dev/null" 2>/dev/null || echo Connection timeout > /dev/null
         done
 }
 
 echo "${RED}[!] Scanning of $ip in progress..."
-scanner $ip $port_number
+scanner $ip $port_number $precision
 echo "${BLUE}[-] Scan finished ! : $port ports scanned"
+
